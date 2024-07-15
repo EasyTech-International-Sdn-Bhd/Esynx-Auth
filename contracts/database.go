918e64afa4802e14c9a9a82a5ce3fc380209a1c7
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-07-15 10:36:19 +0800

    - added logger support - added logger interface

--- contracts/database.go
+++ contracts/database.go
@@ -3,6 +3,7 @@ package contracts
 import (
 	"github.com/easytech-international-sdn-bhd/esynx-auth/models"
 	"github.com/easytech-international-sdn-bhd/esynx-auth/options"
+	"xorm.io/xorm/log"
 )
 
 type IUserSession interface {
@@ -12,11 +13,28 @@ type IUserSession interface {
 	GetConnection() string
 	GetJwtSecret() string
 	GetRedisConfig() models.RedisConfig
-	GetLogger() *interface{}
+	GetLogger() *IDatabaseLogger
 }
 
 type IDatabase interface {
-	Open(conn string, logger *interface{}) error
+	Open(conn string, logger *IDatabaseLogger) error
 	DefineSchema() error
 	Close() error
+}
+
+type IDatabaseLogger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Warn(v ...interface{})
+	Warnf(format string, v ...interface{})
+	Level() log.LogLevel
+	SetLevel(level log.LogLevel)
+	ShowSQL(show ...bool)
+	IsShowSQL() bool
+	BeforeSQL(context *log.LogContext)
+	AfterSQL(context *log.LogContext)
 }
