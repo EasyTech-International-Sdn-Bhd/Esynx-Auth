eab963939c178ef5862ea3bfa5ff1ce7fd759975
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-07-16 09:33:48 +0800

    - refresh token

--- contracts/rbac_authenticate.go
+++ contracts/rbac_authenticate.go
@@ -4,5 +4,5 @@ import "github.com/easytech-international-sdn-bhd/esynx-auth/models"
 
 type IRbacAuthenticate interface {
 	Authenticate(userName, password string) (*models.Authenticated, error)
-	RefreshAuthentication(refreshToken string) (*models.Authenticated, error)
+	RefreshAuthentication(refreshToken string) (*models.RefreshAuthentication, error)
 }
